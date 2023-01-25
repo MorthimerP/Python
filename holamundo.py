5514892afadd54f3a1187e9e6ce10297f0f79d33
MorthimerP <123593376+MorthimerP@users.noreply.github.com>
2023-01-25 15:20:31 -0600

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,12 @@
+# Comentario de una linea: Hola mundo
+print("Hola mundo")
+"""
+Comentario multilinea 1    
+"""
+print('Hola python')
+'''
+Comentario multilinea 2
+'''
+#Consultar tipo de dato
+print(type(3.12345678))
+print(type("palabra"))
